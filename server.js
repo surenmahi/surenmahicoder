928062c7939b1def269dbe19c1f3479d0a9fd717
surenmahi <36105235+surenmahi@users.noreply.github.com>
2018-03-15 12:57:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 var express = require('express');
 var app = express();
-var logger = require('morgan');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 var path = require('path');
-var http = require('http');
-
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'/client')));
 
+app.route('/*')
+  .get(function(req,res){
+    res.sendFile(__dirname + '/client/index.html');
+  });
 
-// app.listen(3000,function(req,res){
-//   console.log('Server is running on port 3000');
-// });
 app.set('port',(process.env.PORT || 3000));
 app.listen(app.get('port'),function(){
-  console.log('Server is running');
+  console.log("Server started");
 });
-// http.createServer(function (req,res){
-//   res.writeHead(200,{'Content-Type':'text/plain'});
-//   res.end('Hello');
-// }).listen(3000,'103.81.236.222');
-// app.listen(3000,'103.81.236.222' || 'localhost', function(){
-//   console.log('Server started');
-// });
